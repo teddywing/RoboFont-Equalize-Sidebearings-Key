76e0ea2b4dab01bef29e9a18589a32897b49de45
Teddy Wing <git@teddywing.com>
2015-11-19 20:48:04 -0500

    equalize_sidebearings.py: Remove commented inspection code

--- equalize_sidebearings.py
+++ equalize_sidebearings.py
@@ -6,13 +6,6 @@ class EqualizeSidebearings(object):
         addObserver(self, 'center', 'spaceCenterKeyUp')
 
     def center(self, info):
-        # print info
-        # self.w = vanilla.Window((400, 400, 1000, 900))
-        # self.w.text = vanilla.TextBox((10, 10, -10, 900), str(dir(info['spaceCenter'])))
-        # self.w.b = vanilla.EditText((0, 0, -0, -0), info['event'].characters())
-        # self.w.text = vanilla.TextBox((10, 10, -10, 900), info['event'].characters)
-        # self.w.open()
-        
         if info['event'].characters() == 'a':
             info['glyph'].center()
 

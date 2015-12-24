caba4fd1713abc46b9d443d313ea53714e43637d
Teddy Wing <git@teddywing.com>
2015-12-24 11:54:37 -0800

    preferences.py: Style window and controls
    * Make the window a bit smaller because there's only one control
    * Add a text label to the editable input
    * Set a size for the editable input and move it into position to the
      right of the text label

--- preferences.py
+++ preferences.py
@@ -3,8 +3,9 @@ import vanilla
 class EqualizeSidebearingsPreferences(object):
 
     def __init__(self):
-        self.w = vanilla.Window((200, 70), 'Equalize Sidebearings')
-        self.w.activation_key = vanilla.EditText((0, 0, 0, 0), 'e')
+        self.w = vanilla.Window((150, 50), 'Equalize Sidebearings')
+        self.w.activation_key_label = vanilla.TextBox((10, 15, -10, 22), 'Short Key:')
+        self.w.activation_key = vanilla.EditText((82, 12, -10, 25), 'e')
         self.w.open()
 
 EqualizeSidebearingsPreferences()
